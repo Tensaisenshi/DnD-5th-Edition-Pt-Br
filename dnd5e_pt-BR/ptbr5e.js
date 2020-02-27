ece20c7e7837488d2f3a768a4a9a114bce9f47d7
Caua539 <caua539@live.com>
2020-02-27 09:09:07 -0300

    Adição de traduções extras para a ficha de personagem, de NPC e de itens; Adição de integração com o módulo babele para traduzir o compêndio de magias (srd); Tradução de strings extras não localizadas; Substituição da ficha original pela ficha localizada quando o idioma é mudado para português;

--- dnd5e_pt-BR/ptbr5e.js
+++ dnd5e_pt-BR/ptbr5e.js
@@ -5,26 +5,206 @@ import { DND5E } from "../../systems/dnd5e/module/config.js";
 import { Dice5e } from "../../systems/dnd5e/module/dice.js";
 import { Actor5e } from "../../systems/dnd5e/module/actor/entity.js";
 import { ActorSheet5eCharacter } from "../../systems/dnd5e/module/actor/sheets/character.js";
+import { ActorSheet5eNPC } from "../../systems/dnd5e/module/actor/sheets/npc.js";
 import { Item5e } from "../../systems/dnd5e/module/item/entity.js";
+import { AbilityUseDialog } from "../../systems/dnd5e/module/apps/ability-use-dialog.js";
+import { AbilityTemplate } from "../../systems/dnd5e/module/pixi/ability-template.js";
 import { ItemSheet5e } from "../../systems/dnd5e/module/item/sheet.js";
 
 Hooks.once('init', () => loadTemplates([
-	'modules/dnd5e_pt-BR/templates/parts/actor-inventory.html',
-	'modules/dnd5e_pt-BR/templates/parts/actor-features.html',
-	'modules/dnd5e_pt-BR/templates/parts/actor-spellbook.html',
-	'modules/dnd5e_pt-BR/templates/parts/actor-traits.html'
+	'modules/dnd5e_pt-BR/templates/actors/parts/actor-inventory.html',
+	'modules/dnd5e_pt-BR/templates/actors/parts/actor-features.html',
+	'modules/dnd5e_pt-BR/templates/actors/parts/actor-spellbook.html',
+	'modules/dnd5e_pt-BR/templates/actors/parts/actor-traits.html',
+	'modules/dnd5e_pt-BR/templates/items/parts/item-action.html',
+	'modules/dnd5e_pt-BR/templates/items/parts/item-activation.html',
+	'modules/dnd5e_pt-BR/templates/items/parts/item-description.html'
 ]));
 
-Hooks.once('init', function() {
-    Actors.registerSheet('dnd5e', PtBr5eSheet, {
-		types: ['character']
-    });
+//Translate non localized strings from the DND5E.CONFIG
+Hooks.once('ready', function() {
+	let lang = game.i18n.lang;
+	if (lang === "pt-BR") {
+		DND5E.armorProficiencies = {
+			"lgt": "Armaduras Leves",
+			"med": "Armaduras Médias",
+			"hvy": "Armaduras Pesadas",
+			"shl": "Escudos"
+		};
+		DND5E.weaponProficiencies = {
+			"sim": "Armas Simples",
+			"mar": "Armas Marciais"
+		};
+		DND5E.toolProficiencies = {
+			"art": "Ferramentas de artesão",
+			"disg": "Kit de disfarce",
+			"forg": "Kit de falsificação",
+			"game": "Kit de jogos",
+			"herb": "Kit de herbalismo",
+			"music": "Instrumento musical",
+			"navg": "Ferramentas de navegador",
+			"pois": "Kit de veneno",
+			"thief": "Ferramentas de ladrão",
+			"vehicle": "Veículos (terra ou água)"
+		};
+		DND5E.abilityActivationTypes = {
+			"none": "DND5E.None",
+			"action": "Ação",
+			"bonus": "Ação Bônus",
+			"reaction": "Reação",
+			"minute": "Minuto",
+			"hour": "Hora",
+			"day": "Dia",
+			"special": "Especial",
+			"legendary": "Lendária",
+			"lair": "Covil"
+		};
+		DND5E.actorSizes = {
+			"tiny": "Minúsculo",
+			"sm": "Pequeno",
+			"med": "Médio",
+			"lg": "Grande",
+			"huge": "Enorme",
+			"grg": "Gigantesco"
+		};
+		DND5E.proficiencyLevels = {
+			0: "Não Proficiente",
+			1: "Proficiente",
+			0.5: "Pau pra Toda Obra",
+			2: "Especialista"
+		};
+	
+		DND5E.languages = {
+			"common": "Comum",
+			"aarakocra": "Aarakocra",
+			"abyssal": "Abissal",
+			"aquan": "Aquan",
+			"auran": "Auran",
+			"celestial": "Celestial",
+			"deep": "Dialeto Obscuro",
+			"draconic": "Dracônico",
+			"druidic": "Druídico",
+			"dwarvish": "Anão",
+			"elvish": "Élfico",
+			"giant": "Gigante",
+			"gith": "Gith",
+			"gnomish": "Gnômico",
+			"goblin": "Goblin",
+			"gnoll": "Gnoll",
+			"halfling": "Pequenino",
+			"ignan": "Ignan",
+			"infernal": "Infernal",
+			"orc": "Orc",
+			"primordial": "Primordial",
+			"sylvan": "Silvestre",
+			"terran": "Terran",
+			"cant": "Gíria de Ladrão",
+			"undercommon": "Subcomum"
+		};
+	
+		DND5E.characterFlags = {
+			"weaponCriticalThreshold": {
+			  name: "Extensão de Acerto Crítico com Arma",
+			  hint: "Permite uma maior chance de acerto crítico; por exemplo Crítico Aprimorado ou Crítico Superior.",
+			  section: "Características",
+			  type: Number,
+			  placeholder: 20
+			},
+			"powerfulBuild": {
+			  name: "Forma Poderosa",
+			  hint: "Provê capacidade de carga superior.",
+			  section: "Traços Raciais",
+			  type: Boolean
+			},
+			"savageAttacks": {
+			  name: "Ataques Selvagens",
+			  hint: "Adiciona um dado de arma extra em acertos críticos.",
+			  section: "Traços Raciais",
+			  type: Boolean
+			},
+			"initiativeAdv": {
+			  name: "Vantagem em Iniciativa",
+			  hint: "De características do personagem ou itens mágicos.",
+			  section: "Características",
+			  type: Boolean
+			},
+			"initiativeHalfProf": {
+			  name: "Meia Proficiência em Iniciativa",
+			  hint: "De \"Pau pra Toda Obra\" ou \"Atleta Extraordinário\".",
+			  section: "Características",
+			  type: Boolean
+			},
+			"initiativeAlert": {
+			  name: "Talento Alerta",
+			  hint: "Provê +5 em Iniciativa.",
+			  section: "Características",
+			  type: Boolean
+			},
+			"saveBonus": {
+			  name: "Bônus de Salvaguarda",
+			  hint: "Modificador bônus para todas as Salvaguardas (ex. +1).",
+			  section: "Características",
+			  type: Number,
+			  placeholder: "+0"
+			},
+			"spellDCBonus": {
+			  name: "Bônus em CD de Magia",
+			  hint: "Modifica o CD de conjuração normal.",
+			  section: "Características",
+			  type: Number,
+			  placeholder: "+0"
+			}
+		  };
+	}
+	
 });
 
-export class PtBr5eSheet extends ActorSheet5eCharacter {
+Hooks.once('init', () => {
+
+	if(typeof Babele !== 'undefined') {
+		
+		Babele.get().register({
+			module: 'dnd5e_pt-BR',
+			lang: 'pt-BR',
+			dir: 'compendium'
+		});
+
+		Babele.get().registerConverters({
+			"weight": (value) => { return parseInt(value) / 2 },
+			"range": (range) => {
+				if(range) {
+					if(range.units === 'ft') {
+						if(range.long) {
+							range = mergeObject(range, { long: range.long * 0.3 });
+						}
+						return mergeObject(range, { value: range.value * 0.3 });
+					}
+					if(range.units === 'mi') {
+						if(range.long) {
+							range = mergeObject(range, { long: range.long * 1.5 });
+						}
+						return mergeObject(range, { value: range.value * 1.5 });
+					}
+					return range;
+				}
+			},
+			"target": (target) => {
+				if(target) {
+					if(target.units === 'ft') {
+						return mergeObject(target, { value: target.value * 0.3 });
+					}
+					return target;
+				}
+			}
+
+		});
+	}
+});
+
+export class PtBrSheet5eCharacter extends ActorSheet5eCharacter {
 	get template() {
-		if ( !game.user.isGM && this.actor.limited ) return super.template;
-		return "modules/dnd5e_pt-BR/templates/ptbr-sheet.html";
+		if ( !game.user.isGM && this.actor.limited ) return "modules/dnd5e_pt-BR/templates/actors/limited-sheet.html";
+		return "modules/dnd5e_pt-BR/templates/actors/character-sheet.html";
 	}
 	
 	static get defaultOptions() {
@@ -227,3 +407,118 @@ export class PtBr5eSheet extends ActorSheet5eCharacter {
 		});
 	}
 }
+
+export class PtBrSheet5eNPC extends ActorSheet5eNPC {
+	static get defaultOptions() {
+		return mergeObject(super.defaultOptions, {
+			  classes: ["ptbr5e", "dnd5e", "sheet", "actor", "npc"],
+			  width: 700,
+			  height: 700
+		  });
+	}
+
+	get template() {
+		if ( !game.user.isGM && this.actor.limited ) return "modules/dnd5e_pt-BR/templates/actors/limited-sheet.html";
+		return "modules/dnd5e_pt-BR/templates/actors/npc-sheet.html";
+	}
+
+	_prepareItems(data) {
+
+		// Categorize Items as Features and Spells
+		const features = {
+		  weapons: { label: "Ataques", items: [] , hasActions: true, dataset: {type: "weapon", "weapon-type": "natural"} },
+		  actions: { label: "Ações", items: [] , hasActions: true, dataset: {type: "feat", "activation.type": "action"} },
+		  passive: { label: "Características", items: [], dataset: {type: "feat"} },
+		  equipment: { label: "Inventário", items: [], dataset: {type: "loot"}}
+		};
+	
+		// Start by classifying items into groups for rendering
+		let [spells, other] = data.items.reduce((arr, item) => {
+		  item.img = item.img || DEFAULT_TOKEN;
+		  item.isStack = item.data.quantity ? item.data.quantity > 1 : false;
+		  item.hasUses = item.data.uses && (item.data.uses.max > 0);
+		  item.isOnCooldown = item.data.recharge && !!item.data.recharge.value && (item.data.recharge.charged === false);
+		  item.isDepleted = item.isOnCooldown && (item.data.uses.per && (item.data.uses.value > 0));
+		  item.hasTarget = !!item.data.target && !(["none",""].includes(item.data.target.type));
+		  if ( item.type === "spell" ) arr[0].push(item);
+		  else arr[1].push(item);
+		  return arr;
+		}, [[], []]);
+	
+		// Apply item filters
+		spells = this._filterItems(spells, this._filters.spellbook);
+		other = this._filterItems(other, this._filters.features);
+	
+		// Organize Spellbook
+		const spellbook = this._prepareSpellbook(data, spells);
+	
+		// Organize Features
+		for ( let item of other ) {
+		  if ( item.type === "weapon" ) features.weapons.items.push(item);
+		  else if ( item.type === "feat" ) {
+			if ( item.data.activation.type ) features.actions.items.push(item);
+			else features.passive.items.push(item);
+		  }
+		  else if (["equipment", "consumable", "tool", "loot"].includes(item.type)) {
+			features.equipment.items.push(item);
+		  }
+		}
+	
+		// Assign and return
+		data.features = Object.values(features);
+		data.spellbook = spellbook;
+	  }
+}
+
+export class PtBrItemSheet5e extends ItemSheet5e {
+	constructor(...args) {
+		super(...args);
+	
+		/**
+		 * The tab being browsed
+		 * @type {string}
+		 */
+		this._sheetTab = null;
+	}
+
+	static get defaultOptions() {
+		return mergeObject(super.defaultOptions, {
+		width: 600,
+		height: 420,
+		classes: ["ptbr5e", "dnd5e", "sheet", "item"],
+		resizable: false,
+		scrollY: [".tab.details"]
+	  });
+	}
+
+	get template() {
+		const path = "modules/dnd5e_pt-BR/templates/items/";
+		return `${path}/${this.item.data.type}.html`;
+	}
+
+	_getItemStatus(item) {
+		if ( item.type === "spell" ) return item.data.preparation.prepared ? "Preparada" : "Despreparada";
+		else if ( ["weapon", "equipment"].includes(item.type) ) return item.data.equipped ? "Equipado" : "Não Equipado";
+		else if ( item.type === "tool" ) return item.data.proficient ? "Proficiente" : "Não Proficiente";
+	  }
+}
+
+Hooks.once('ready', function() {
+	let lang = game.i18n.lang;
+	if (lang === "pt-BR") {
+		Actors.unregisterSheet("dnd5e", ActorSheet5eCharacter);
+		Actors.registerSheet('dnd5e', PtBrSheet5eCharacter, {
+			types: ['character'],
+			makeDefault: true
+		});
+
+		Actors.unregisterSheet("dnd5e", ActorSheet5eNPC);
+		Actors.registerSheet('dnd5e', PtBrSheet5eNPC, {
+			types: ['npc'],
+			makeDefault: true
+		});
+
+		Items.unregisterSheet("dnd5e", ItemSheet5e);
+		Items.registerSheet("dnd5e", PtBrItemSheet5e);
+	}
+});
