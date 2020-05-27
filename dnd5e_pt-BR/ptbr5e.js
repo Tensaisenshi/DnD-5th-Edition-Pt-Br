6aceba68fcdab109602381275f5ea04cbdca0b48
Caua539 <caua539@live.com>
2020-05-27 11:05:34 -0300

    chore: Increment module version

--- dnd5e_pt-BR/ptbr5e.js
+++ dnd5e_pt-BR/ptbr5e.js
@@ -1,6 +1,6 @@
 // DND5E-PTBR
 // @author Caua539
-// @version 0.90.1
+// @version 0.92.0
 import {DND5E} from "../../systems/dnd5e/module/config.js";
 import Actor from "../../systems/dnd5e/module/actor/sheets/character.js";
 import NPC from "../../systems/dnd5e/module/actor/sheets/npc.js";
