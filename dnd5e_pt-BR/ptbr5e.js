0578d4b1342a6849b272a223908c813a8454f5a1
Elvis Pereira <elvispereira@hotmail.com>
2020-09-02 15:19:23 -0300

    feat: tradução da versao 0.96 do sistema dnd5e
    - Atualização dos termos para a versão 0.96 do sistema dnd5e
    - Remocao da pasta lang-reference que nao era mais necessaria
    - Adicão do campo "dependecies" no module.json para definir o babele como uma dependencia
    - Tradução dos compêndios Class Features, Items e Trade Goods adicionadas
    - README.md atualizado
    - Formatação aplicada ao arquivo ptbr5e.js

--- dnd5e_pt-BR/ptbr5e.js
+++ dnd5e_pt-BR/ptbr5e.js
@@ -1,13 +1,13 @@
 // DND5E-PTBR
 // @author Caua539
 // @version 0.93.0
-import {DND5E} from "../../systems/dnd5e/module/config.js";
+import { DND5E } from "../../systems/dnd5e/module/config.js";
 import Actor from "../../systems/dnd5e/module/actor/sheets/character.js";
 import NPC from "../../systems/dnd5e/module/actor/sheets/npc.js";
 import Item from "../../systems/dnd5e/module/item/sheet.js";
 
 //Translate non localized strings from the DND5E.CONFIG
-Hooks.once('ready', function() {
+Hooks.once('ready', function () {
 	let lang = game.i18n.lang;
 	if (lang === "pt-BR") {
 		DND5E.armorProficiencies = {
@@ -29,13 +29,13 @@ Hooks.once('ready', function() {
 			"lair": "Ação de Covil"
 		};
 	}
-	
+
 });
 
 Hooks.once('init', () => {
 
-	if(typeof Babele !== 'undefined') {
-		
+	if (typeof Babele !== 'undefined') {
+
 		Babele.get().register({
 			module: 'dnd5e_pt-BR',
 			lang: 'pt-BR',
@@ -45,15 +45,15 @@ Hooks.once('init', () => {
 		Babele.get().registerConverters({
 			"weight": (value) => { return parseInt(value) / 2 },
 			"range": (range) => {
-				if(range) {
-					if(range.units === 'ft') {
-						if(range.long) {
+				if (range) {
+					if (range.units === 'ft') {
+						if (range.long) {
 							range = mergeObject(range, { long: range.long * 0.3 });
 						}
 						return mergeObject(range, { value: range.value * 0.3 });
 					}
-					if(range.units === 'mi') {
-						if(range.long) {
+					if (range.units === 'mi') {
+						if (range.long) {
 							range = mergeObject(range, { long: range.long * 1.5 });
 						}
 						return mergeObject(range, { value: range.value * 1.5 });
@@ -62,8 +62,8 @@ Hooks.once('init', () => {
 				}
 			},
 			"target": (target) => {
-				if(target) {
-					if(target.units === 'ft') {
+				if (target) {
+					if (target.units === 'ft') {
 						return mergeObject(target, { value: target.value * 0.3 });
 					}
 					return target;
@@ -77,36 +77,36 @@ Hooks.once('init', () => {
 export class ActorSheet5eCharacter extends Actor {
 	static get defaultOptions() {
 		return mergeObject(super.defaultOptions, {
-			  classes: ["ptbr5e", "dnd5e", "sheet", "actor", "character"],
-			  width: 800,
-			  height: 800
-		  });
-	  }
+			classes: ["ptbr5e", "dnd5e", "sheet", "actor", "character"],
+			width: 800,
+			height: 800
+		});
+	}
 }
 
 export class ActorSheet5eNPC extends NPC {
 	static get defaultOptions() {
 		return mergeObject(super.defaultOptions, {
-			  classes: ["ptbr5e", "dnd5e", "sheet", "actor", "npc"],
-			  width: 700,
-			  height: 700
-		  });
+			classes: ["ptbr5e", "dnd5e", "sheet", "actor", "npc"],
+			width: 700,
+			height: 700
+		});
 	}
 }
 
 export class ItemSheet5e extends Item {
 	static get defaultOptions() {
 		return mergeObject(super.defaultOptions, {
-		width: 600,
-		height: 420,
-		classes: ["ptbr5e", "dnd5e", "sheet", "item"],
-		resizable: false,
-		scrollY: [".tab.details"]
-	  });
+			width: 600,
+			height: 420,
+			classes: ["ptbr5e", "dnd5e", "sheet", "item"],
+			resizable: false,
+			scrollY: [".tab.details"]
+		});
 	}
 }
 
-Hooks.once('ready', function() {
+Hooks.once('ready', function () {
 	let lang = game.i18n.lang;
 	if (lang === "pt-BR") {
 		Actors.unregisterSheet("dnd5e", Actor);
